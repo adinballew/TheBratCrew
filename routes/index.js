8d37849ea939d0d7651cd5acd9b46bb290912a2b
adinb <adinballew@gmail.com>
2017-10-03 11:09:51 -0500

    Dynamically generated flavors test. Server deploy regression test

--- routes/index.js
+++ routes/index.js
@@ -13,16 +13,16 @@ const db = pgp(config);
 /* GET home page. */
 router.get('/', (req, res, next) =>
 {
-	db.any('select * from flavors')
-		.then(function (result)
-		{
-			console.log(result);
-			res.render('index', {title: 'theBRATcrew'});
-		})
-		.catch(function (err)
-		{
-			console.log(err);
-		});
+	// db.any('select * from flavors')
+	// 	.then(function (result)
+	// 	{
+	// 		console.log(result);
+	res.render('index', {title: 'theBRATcrew'});
+	// })
+	// .catch(function (err)
+	// {
+	// 	console.log(err);
+	// });
 });
 
 router.get('/contact', (req, res, next) =>
@@ -35,39 +35,53 @@ router.get('/about', (req, res, next) =>
 	res.render('about', {});
 });
 
+/** @class all_flavors
+ *  @property name
+ *  @property description
+ *  @property image_path **/
 router.get('/all_flavors', (req, res, next) =>
 {
-	res.render('all_flavors', {});
+	console.time('test');
+	db.any('select * from flavors')
+		.then(function (result)
+		{
+			res.render('redirects/all_flavors', {all_flavors: result});
+			console.timeEnd('test');
+		})
+		.catch(function (err)
+		{
+			console.log(err);
+		});
 });
 
 router.get('/candy_flavors', (req, res, next) =>
 {
-	res.render('candy_flavors', {});
+	res.render('redirects/candy_flavors', {});
 });
 
 router.get('/desert_flavors', (req, res, next) =>
 {
-	res.render('desert_flavors', {});
+	res.render('redirects/desert_flavors', {});
 });
 
 router.get('/drink_flavors', (req, res, next) =>
 {
-	res.render('drink_flavors', {});
+	res.render('redirects/drink_flavors', {});
 });
 
 router.get('/fruit_flavors', (req, res, next) =>
 {
-	res.render('fruit_flavors', {});
+	res.render('redirects/fruit_flavors', {});
 });
 
 router.get('/special_flavors', (req, res, next) =>
 {
-	res.render('special_flavors', {});
+	res.render('redirects/special_flavors', {});
 });
 
 router.get('/tobacco_flavors', (req, res, next) =>
 {
-	res.render('tobacco_flavors', {});
+	res.render('redirects/tobacco_flavors', {});
 });
 
 module.exports = router;
